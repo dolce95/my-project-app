4eafc86647c48f7e8482df43a2a8f503b0d057ce
Danilo Dolce <dolcedanilo1995@gmail.com>
2022-09-02 16:59:46 +0200

    Lista ricette e inizio dettaglio

--- src/app/ricette/lista-ricette/lista-ricette.component.ts
+++ src/app/ricette/lista-ricette/lista-ricette.component.ts
@@ -1,3 +1,4 @@
+import { Ricetta } from './../ricetta.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lista-ricette.component.css']
 })
 export class ListaRicetteComponent implements OnInit {
+  ricette: Ricetta[] = [
+    new Ricetta("Ricetta test", "Semplice ricetta di test", "https://live.staticflickr.com/2353/2448935929_0cec69aefa_b.jpg"),
+    new Ricetta("Ricetta test", "Semplice ricetta di test", "https://live.staticflickr.com/2353/2448935929_0cec69aefa_b.jpg")
+  ]
 
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.ricette)
   }
 
 }
--- src/app/ricette/ricette.component.ts
+++ src/app/ricette/ricette.component.ts
@@ -6,7 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ricette.component.css']
 })
 export class RicetteComponent implements OnInit {
-  ricette: []
 
   constructor() { }
 
